996f3149142f7e7da197961d4bbed8b156a108e6
Kris1027 <obarzanek.work@gmail.com>
2024-11-07 08:09:51 +0100

    connecting with the database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import connectMongoDB from './db/connect-mongodb.js';
 
 dotenv.config();
 
@@ -15,5 +16,6 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
+    connectMongoDB();
     console.log('Server is running at http://localhost:' + PORT);
 });
