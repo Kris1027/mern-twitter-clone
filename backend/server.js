b897b8a265b50d17b81c700eb3cbb18fee27ca0b
Kris1027 <obarzanek.work@gmail.com>
2024-11-07 08:00:49 +0100

    setup initial auth routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, (req, res) => {
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
     console.log('Server is running at http://localhost:' + PORT);
 });
