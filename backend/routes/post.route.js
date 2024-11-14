7521a22f950407b0a67c6f04350d5f3ce8265ef5
Kris1027 <obarzanek.work@gmail.com>
2024-11-14 16:10:18 +0100

    create likeUnlikePost api endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { commentOnPost, createPost, deletePost } from '../controllers/post.controller.js';
+import {
+    commentOnPost,
+    createPost,
+    deletePost,
+    likeUnlikePost,
+} from '../controllers/post.controller.js';
 import { protectRoute } from '../middleware/protect-route.js';
 
 const router = express.Router();
 
 router.post('/create', protectRoute, createPost);
-router.delete('/:id', protectRoute, deletePost);
+router.post('/like/:id', protectRoute, likeUnlikePost);
 router.post('/comment/:id', protectRoute, commentOnPost);
+router.delete('/:id', protectRoute, deletePost);
 
 export default router;
