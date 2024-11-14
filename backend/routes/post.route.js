81ae80b51e85d6984b2894078091a30a0ecdf1d6
Kris1027 <obarzanek.work@gmail.com>
2024-11-14 16:54:18 +0100

    create getFollowingPosts api endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,6 +4,7 @@ import {
     createPost,
     deletePost,
     getAllPosts,
+    getFollowingPosts,
     getLikedPosts,
     likeUnlikePost,
 } from '../controllers/post.controller.js';
@@ -12,6 +13,7 @@ import { protectRoute } from '../middleware/protect-route.js';
 const router = express.Router();
 
 router.get('/all', protectRoute, getAllPosts);
+router.get('/following', protectRoute, getFollowingPosts);
 router.get('/likes/:id', protectRoute, getLikedPosts);
 router.post('/create', protectRoute, createPost);
 router.post('/like/:id', protectRoute, likeUnlikePost);
