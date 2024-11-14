525ebe2548ec9398994af5712591cd94e1905da2
Kris1027 <obarzanek.work@gmail.com>
2024-11-14 14:03:41 +0100

    create commentOnPost api endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createPost, deletePost } from '../controllers/post.controller.js';
+import { commentOnPost, createPost, deletePost } from '../controllers/post.controller.js';
 import { protectRoute } from '../middleware/protect-route.js';
 
 const router = express.Router();
 
 router.post('/create', protectRoute, createPost);
 router.delete('/:id', protectRoute, deletePost);
+router.post('/comment/:id', protectRoute, commentOnPost);
 
 export default router;
