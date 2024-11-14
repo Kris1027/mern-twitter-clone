ab40bf05094357976ef7bd57ffda907f0059fe3a
Kris1027 <obarzanek.work@gmail.com>
2024-11-14 16:26:34 +0100

    create getAllPosts api endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -3,12 +3,14 @@ import {
     commentOnPost,
     createPost,
     deletePost,
+    getAllPosts,
     likeUnlikePost,
 } from '../controllers/post.controller.js';
 import { protectRoute } from '../middleware/protect-route.js';
 
 const router = express.Router();
 
+router.get('/all', protectRoute, getAllPosts);
 router.post('/create', protectRoute, createPost);
 router.post('/like/:id', protectRoute, likeUnlikePost);
 router.post('/comment/:id', protectRoute, commentOnPost);
