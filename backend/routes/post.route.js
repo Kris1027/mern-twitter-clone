f6f8d4d1f4387a6d29318dce2e859196570b413a
Kris1027 <obarzanek.work@gmail.com>
2024-11-14 16:41:58 +0100

    create getLikedPosts api endpoint

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,6 +4,7 @@ import {
     createPost,
     deletePost,
     getAllPosts,
+    getLikedPosts,
     likeUnlikePost,
 } from '../controllers/post.controller.js';
 import { protectRoute } from '../middleware/protect-route.js';
@@ -11,6 +12,7 @@ import { protectRoute } from '../middleware/protect-route.js';
 const router = express.Router();
 
 router.get('/all', protectRoute, getAllPosts);
+router.get('/likes/:id', protectRoute, getLikedPosts);
 router.post('/create', protectRoute, createPost);
 router.post('/like/:id', protectRoute, likeUnlikePost);
 router.post('/comment/:id', protectRoute, commentOnPost);
