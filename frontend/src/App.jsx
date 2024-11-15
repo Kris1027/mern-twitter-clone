a844a3b0056745329144328ed4fbf2da0002276b
Kris1027 <obarzanek.work@gmail.com>
2024-11-15 13:35:27 +0100

    build sidebar component and fix svg import in login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/auth/home/home-page';
 import LoginPage from './pages/auth/login/login-page';
 import SignUpPage from './pages/auth/signup/sign-up-page';
+import Sidebar from './components/common/sidebar';
 
 function App() {
     return (
         <div className='flex max-w-6xl mx-auto'>
+            {/* Common component, bc its not wrapped with Routes */}
+            <Sidebar />
             <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='/signup' element={<SignUpPage />} />
