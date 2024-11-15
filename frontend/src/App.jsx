cbe00d1f5bc8e18d45006b522cc224a556787694
Kris1027 <obarzanek.work@gmail.com>
2024-11-15 10:19:13 +0100

    create auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/auth/home/home-page';
+import { LoginPage } from './pages/auth/login/login-page';
+import { SignUpPage } from './pages/auth/signup/sign-up-page';
+
 function App() {
     return (
-        <>
-            <p>Hello</p>
-        </>
+        <div className='flex max-w-6xl mx-auto'>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/signup' element={<SignUpPage />} />
+                <Route path='/login' element={<LoginPage />} />
+            </Routes>
+        </div>
     );
 }
 
