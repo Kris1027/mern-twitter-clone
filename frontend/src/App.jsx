be7f077b4995c6658d3215f28c716aa3d5581d7a
Kris1027 <obarzanek.work@gmail.com>
2024-11-15 13:46:59 +0100

    create right panel component and right panel skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
+
+import RightPanel from './components/common/right-panel';
+import Sidebar from './components/common/sidebar';
+
 import HomePage from './pages/auth/home/home-page';
 import LoginPage from './pages/auth/login/login-page';
 import SignUpPage from './pages/auth/signup/sign-up-page';
-import Sidebar from './components/common/sidebar';
 
 function App() {
     return (
@@ -14,6 +17,7 @@ function App() {
                 <Route path='/signup' element={<SignUpPage />} />
                 <Route path='/login' element={<LoginPage />} />
             </Routes>
+            <RightPanel />
         </div>
     );
 }
