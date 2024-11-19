caf41f0ee408f1d05fc903fdc77503d2310defa1
Kris1027 <obarzanek.work@gmail.com>
2024-11-19 12:50:43 +0100

    fetch user profile

--- frontend/src/pages/profile/profile-page.tsx
+++ frontend/src/pages/profile/profile-page.tsx
@@ -1,9 +1,10 @@
-import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 import Posts from '../../components/common/posts';
 import ProfileHeaderSkeleton from '../../components/skeletons/profile-header-skeleton';
 import EditProfileModal from './edit-profile-modal';
+import { formatMemberSinceDate } from '../../utils/date';
 
 import { POSTS } from '../../utils/db/dummy';
 
@@ -11,29 +12,38 @@ import { FaArrowLeft } from 'react-icons/fa6';
 import { IoCalendarOutline } from 'react-icons/io5';
 import { FaLink } from 'react-icons/fa';
 import { MdEdit } from 'react-icons/md';
+import { useQuery } from '@tanstack/react-query';
 
 const ProfilePage = () => {
     const [coverImg, setCoverImg] = useState(null);
     const [profileImg, setProfileImg] = useState(null);
     const [feedType, setFeedType] = useState('posts');
 
+    const { username } = useParams();
+
     const coverImgRef = useRef(null);
     const profileImgRef = useRef(null);
 
-    const isLoading = false;
     const isMyProfile = true;
 
-    const user = {
-        _id: '1',
-        fullName: 'John Doe',
-        username: 'johndoe',
-        profileImg: '/avatars/boy2.png',
-        coverImg: '/cover.png',
-        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        link: 'https://youtube.com/@asaprogrammer_',
-        following: ['1', '2', '3'],
-        followers: ['1', '2', '3'],
-    };
+    const {
+        data: user,
+        isLoading,
+        refetch,
+        isRefetching,
+    } = useQuery({
+        queryKey: ['userProfile'],
+        queryFn: async () => {
+            try {
+                const res = await fetch(`/api/users/profile/${username}`);
+                const data = await res.json();
+                if (!res.ok) throw new Error(data.error || 'Something went wrong');
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    });
 
     const handleImgChange = (e, state) => {
         const file = e.target.files[0];
@@ -47,14 +57,20 @@ const ProfilePage = () => {
         }
     };
 
+    useEffect(() => {
+        refetch();
+    }, [username, refetch]);
+
     return (
         <>
             <div className='flex-[4_4_0]  border-r border-gray-700 min-h-screen '>
                 {/* HEADER */}
-                {isLoading && <ProfileHeaderSkeleton />}
-                {!isLoading && !user && <p className='text-center text-lg mt-4'>User not found</p>}
+                {(isLoading || isRefetching) && <ProfileHeaderSkeleton />}
+                {!isLoading && !isRefetching && !user && (
+                    <p className='text-center text-lg mt-4'>User not found</p>
+                )}
                 <div className='flex flex-col'>
-                    {!isLoading && user && (
+                    {!isLoading && !isRefetching && user && (
                         <>
                             <div className='flex gap-10 px-4 py-2 items-center'>
                                 <Link to='/'>
@@ -153,12 +169,12 @@ const ProfilePage = () => {
                                             <>
                                                 <FaLink className='w-3 h-3 text-slate-500' />
                                                 <a
-                                                    href='https://youtube.com/@asaprogrammer_'
+                                                    href={user?.link}
                                                     target='_blank'
                                                     rel='noreferrer'
                                                     className='text-sm text-blue-500 hover:underline'
                                                 >
-                                                    youtube.com/@asaprogrammer_
+                                                    {user?.link}
                                                 </a>
                                             </>
                                         </div>
@@ -166,7 +182,7 @@ const ProfilePage = () => {
                                     <div className='flex gap-2 items-center'>
                                         <IoCalendarOutline className='w-4 h-4 text-slate-500' />
                                         <span className='text-sm text-slate-500'>
-                                            Joined July 2021
+                                            {formatMemberSinceDate(user?.createdAt)}
                                         </span>
                                     </div>
                                 </div>
