1e593cd371f0b5a44ede441b7cca8ed000586eaf
Kris1027 <obarzanek.work@gmail.com>
2024-11-19 13:42:43 +0100

    fix bugs in right panel

--- frontend/src/components/common/right-panel.jsx
+++ frontend/src/components/common/right-panel.jsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
-import RightPanelSkeleton from '../skeletons/right-panel-skeletons';
 import { useQuery } from '@tanstack/react-query';
+
 import useFollow from '../../hooks/use-follow';
+
+import RightPanelSkeleton from '../skeletons/right-panel-skeletons';
 import LoadingSpinner from './loading-spinner';
 
 const RightPanel = () => {
@@ -9,9 +11,9 @@ const RightPanel = () => {
         queryKey: ['suggestedUsers'],
         queryFn: async () => {
             try {
-                const res = await fetch('api/users/suggested');
-                const data = res.json();
-                if (!res.ok) throw new Error(data.error || 'Something went wrong');
+                const res = await fetch('/api/users/suggested');
+                const data = await res.json();
+                if (!res.ok) throw new Error(data.error || 'Something went wrong!');
                 return data;
             } catch (error) {
                 throw new Error(error.message);
@@ -21,7 +23,7 @@ const RightPanel = () => {
 
     const { follow, isPending } = useFollow();
 
-    if (suggestedUsers?.length === 0) return <div className='md:w-64 w0'></div>;
+    if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
     return (
         <div className='hidden lg:block my-4 mx-2'>
